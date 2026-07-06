2b071b393399f4cc5e7bc886527c63b5c99c481c
AshishResolute <anshugourh21@gmail.com>
2026-07-06 11:58:57 +0530

    Render cache deployment issue checking if this code changes works

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
-import { PORT } from './config/config.js';
-import {app,} from './routes/app.js';
+import { PORT } from "./config/config.js";
+import { app } from "./routes/app.js";
 
-
-
-app.listen(PORT,()=>{
-    console.log(`Server running on http://localhost:${PORT}`);
-    
-})
+app.listen(PORT, () => {
+  console.log(`Server running on https://tracker-l40e.onrender.com/`);
+});
